2ef2b39982b45f038c00e4270e0bfb09328b5bca
skywo1f <ivan_viti@hotmail.com>
2021-06-05 15:51:46 -0400

    Update flagBase_h.py
    fixed base colors

--- flagBase_h.py
+++ flagBase_h.py
@@ -12,25 +12,28 @@ class flagBase:
     x = 0
     y = 0
     teamCount = [0]*nTeams                                  #how many players of each team are near the goal
-    
+    timeOfLastSpawn = 0
     def __init__(self,x,y,team):
         self.x = x
         self.y = y
         self.team = team
-        
-    def updateTeam(self,npcList):
+        self.color = teamToColor(team)
+    def updateTeam(self,npcList,playerX,playerY):
         for i in range(nTeams):                     #first reset the list
             self.teamCount[i] = 0
-            
+        r = getRange(self.x,self.y,playerX,playerY)
+        if r < baseRange:
+            self.teamCount[0] = self.teamCount[0] + 1
         for i in range(len(npcList)):
             r = getRange(self.x,self.y,npcList[i].x,npcList[i].y)
             if r < baseRange:
-                self.teamCount[npcList[i]] = self.teamCount[npcList[i]]  + 1
+                self.teamCount[npcList[i].team] = self.teamCount[npcList[i].team]  + 1
         maxNum = 0
         for i in range(nTeams):                                         #could technically be folded into the last nteams loop but im putting it here for simplicity
             if self.teamCount[i] > maxNum:
-                self.team = i
-                maxNum = self.teamCount[i]
-                
+                if self.teamCount[i] > 0:
+                    self.team = i
+                    maxNum = self.teamCount[i]
+                    self.color = teamToColor(self.team)
     def drawBase(self):
-        pygame.draw.rect(screen, GREY, [1 + self.x, 1 + self.y, 10, 10], 2)                         #GREY will eventually be switched with teamColors
+        pygame.draw.rect(screen, self.color, [1 + self.x, 1 + self.y, 10, 10], 2)                         #GREY will eventually be switched with teamColors
