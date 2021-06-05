6f0ab78be3bdb9d462029ed67fdc441745c5ecde
skywo1f <ivan_viti@hotmail.com>
2021-06-05 15:52:26 -0400

    Update utilities_h.py
    added team to color function

--- utilities_h.py
+++ utilities_h.py
@@ -11,3 +11,10 @@ def getRange(x1,y1,x2,y2):
     deltaY = y2-y1
     r = math.sqrt(deltaX*deltaX + deltaY*deltaY)
     return r
+
+def teamToColor(team):
+    redPart =abs((team*11719  )%255 - 40)               #first make it pseudorandom then subtract 40 so its not too white  then abs so its not negative
+    greenPart = abs((team*12451  )%255 - 40)
+    bluePart = abs((team*12953  )%255 - 40)
+    color = (redPart,greenPart,bluePart)
+    return color
